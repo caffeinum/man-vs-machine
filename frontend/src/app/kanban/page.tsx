2fdc281c21adbfd755d7b489f099482ab8b8c5af
Aleksey Bykhun <theisease@gmail.com>
2025-09-06 14:49:33 -0700

    improve kanban board ui

--- frontend/src/app/kanban/page.tsx
+++ frontend/src/app/kanban/page.tsx
@@ -4,12 +4,8 @@ import KanbanBoard from "../_components/board";
 export default async function Home() {
 	return (
 		<HydrateClient>
-			<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#ffd190] to-[#fffff4] text-white">
+			<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#ffd190] to-[#fffff4]">
 				<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-					<h1 className="font-extrabold text-5xl tracking-tight sm:text-[5rem]">
-						Agents Kanban
-					</h1>
-
 					<KanbanBoard />
 				</div>
 			</main>
