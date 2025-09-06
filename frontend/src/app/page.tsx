17fb34d8b66be17982ba4c9fd9225d0c960923e9
Aleksey Bykhun <theisease@gmail.com>
2025-09-06 14:48:30 -0700

    biome

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,7 @@ import { HydrateClient } from "~/trpc/server";
 export default async function Home() {
 	return (
 		<HydrateClient>
-			<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b to-[#01ffb7] from-[#ffe6ac]">
+			<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#ffe6ac] to-[#01ffb7]">
 				<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
 					<Header />
 					<h1 className="font-extrabold text-5xl tracking-tight sm:text-[5rem]">
